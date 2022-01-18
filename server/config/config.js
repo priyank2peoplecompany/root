63167c2fbdd4fb5df00801fbedc74fbc0345ba38
priyanktwopeoplecompany <priyank@twopeople.company>
2022-01-18 17:33:47 +0530

    update config file

--- server/config/config.js
+++ server/config/config.js
@@ -10,8 +10,8 @@ const DB_DETAILS = {
 	"development": {
 		"db_host": "localhost",
 		"db_name": "root",
-		"db_username": "root_db_user",
-		"db_passoword": "",
+		"db_username": "admin",
+		"db_passoword": "pSeW5VZ2Edgc",
 		"db_dialect": "",
 		"multipleStatements": true
 	},
@@ -27,23 +27,6 @@ const DB_DETAILS = {
 
 const env_db = DB_DETAILS[process.env.NODE_ENV];
 
-
-//const mongoURI = "mongodb+srv://MyUserName :<MyPassword>@cluster0.lbb8n.mongodb.net/<DbImUsing>?retryWrites=true&w=majority";
-
-
 //mongoose.connect(`mongodb+srv://${env_db.db_username}:${env_db.db_password}@rootapp.nxvir.mongodb.net/${env_db.db_name}?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true });
 mongoose.connect(`mongodb+srv://root_db_user:wHTEc4WubhZSP9k5@rootapp.nxvir.mongodb.net/rootapp?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true });
 //mongoose.connect(`mongodb+srv://${env_db.db_host}:27017/${env_db.db_name}`, { useUnifiedTopology: true, useNewUrlParser: true });
-
-
-
-// const { MongoClient } = require('mongodb');
-// const uri = "mongodb+srv://root_db_user:wHTEc4WubhZSP9k5@rootapp.nxvir.mongodb.net/rootapp?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   if(err){
-// 	  console.log("Connection error=====>",err);
-//   }
-//   // perform actions on the collection object
-//   client.close();
-// });
